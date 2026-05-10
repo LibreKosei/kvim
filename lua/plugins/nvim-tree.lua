17dd9e74059c32dc0a26b61a6fff02c5e0f3db14
LibreKosei <cryptokell@tutanota.com>
2026-05-10 20:41:06 +0800

    feat: space + e to focus back to nvim tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,7 +2,8 @@ require("lz.n").load {
     {
         "nvim-tree.lua",
         keys = {
-          { "<C-n>", "<CMD>NvimTreeToggle<CR>", desc = "Toggle NvimTree" },
+            { "<C-n>", "<CMD>NvimTreeToggle<CR>", desc = "Toggle NvimTree" },
+            { "<leader>e", "<CMD>NvimTreeFocus<CR>", desc = "Focus back to nvim tree"},
         },
         after = function ()
             require("nvim-tree").setup({
