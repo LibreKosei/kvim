2c9475baf19072e210b0ad1cd8aeaf4f21add038
LibreKosei <cryptokell@tutanota.com>
2026-05-11 17:13:01 +0800

    update: pyright settings

--- lua/lsp/pyright.lua
+++ lua/lsp/pyright.lua
@@ -2,6 +2,14 @@ vim.lsp.config('pyright', {
     cmd = { 'pyright-langserver', '--stdio' },
     filetypes = { 'python' },
     root_markers = { "pyrightconfig.json", "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
+    settings = {
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+            },
+        },
+    },
 })
 
 vim.api.nvim_create_autocmd('FileType', {
