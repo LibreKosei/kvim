c7d9d89d86348e5298c65b7a3906f4ef0d0b94dd
LibreKosei <cryptokell@tutanota.com>
2026-06-25 11:29:32 +0800

    update: auto-completition settings
    enabled preset keybinds

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -5,11 +5,15 @@ require("lz.n").load {
         after = function ()
             require("blink.cmp").setup({
                 keymap = {
+                    preset = 'default',
                     ['<CR>'] = { 'accept', 'fallback' },
                     ['<Tab>'] = { 'select_next', 'fallback' },
-                    ['<S-Tab>'] = { 'select_prev', 'fallback' }
+                    ['<S-Tab>'] = { 'select_prev', 'fallback' },
                 },
                 completion = {
+                    menu = {
+                        auto_show = false,
+                    },
                     documentation = {
                         auto_show = true,
                         auto_show_delay_ms = 500,
@@ -18,9 +22,15 @@ require("lz.n").load {
                         selection = {
                             preselect = false,
                             auto_insert = false,
-                        }
+                        },
                     },
-                }
+                },
+                snippets = {
+                    preset = 'luasnip',
+                },
+                sources = {
+                    default = { "lsp", "path", "snippets", },
+                },
             })
         end,
     }
